98b0d26cc1f945772b7033959957cef3dacc07c9
Durgesh Bhalkar <durgeshbhalkar.com>
2025-09-26 22:30:31 +0530

    ready to deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,28 @@
 
 import express from "express";  
 import dotenv from "dotenv";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js"
 import massageRoutes from "./routes/massage.route.js";
 
 dotenv.config();
 const app=express();
+const __dirname = path.resolve();
+
 const PORT=process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/massages", massageRoutes);
+
+//Make ready for deployment
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    });
+}
 
 app.listen(3000, ()=> console.log("Server is runing on poart no 3000 555 " + PORT));
